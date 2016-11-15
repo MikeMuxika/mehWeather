3ebf294bf2cc15ddbde2b843b94de6717fe762ea
muxica <michael.a.stearn@gmail.com>
2016-11-15 18:05:55 -0500

    forgot to convert http to https in middleware

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'http') {
+  if (req.headers['x-forwarded-proto'] === 'https') {
     res.redirect('http://' + req.hostname + req.url);
   } else {
     next();
